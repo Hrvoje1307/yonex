6d004df2c3f3a80a194f1a31553e86074daf1ec1
Hrvooje <cuckovichrvoje@gmail.com>
2024-10-14 16:09:08 +0200

    a

--- app/config/database.sql
+++ app/config/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2024 at 03:52 PM
+-- Generation Time: Oct 14, 2024 at 04:08 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -1745,6 +1745,34 @@ INSERT INTO `cords` (`id`, `name`, `price`, `priceNOTAX`, `img_url`, `thicknesse
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` mediumtext NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `fullName` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `phoneNumber` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `postcode` varchar(200) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `isSent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `order_id`, `user_id`, `product_id`, `quantity`, `fullName`, `email`, `phoneNumber`, `address`, `postcode`, `city`, `isSent`) VALUES
+(16, 1, 18, '106004', 1, 'Hrvoje Čučković', 'cuckovichrvoje@gmail.com', '0957056320', 'Vilima Korajca 10', '10090', 'Zagreb', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rackets`
 --
 
@@ -2356,6 +2384,12 @@ ALTER TABLE `cords`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `rackets`
 --
 ALTER TABLE `rackets`
@@ -2401,6 +2435,12 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `checkboxfilters`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
